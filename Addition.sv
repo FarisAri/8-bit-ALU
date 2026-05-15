4e793995c15aebc6e15c77ac330ba0e543a80ecb
Trenton25 <tdog2557@gmail.com>
2026-05-15 15:28:16 -0700

    Worked on addition and subtration

--- Addition.sv
+++ Addition.sv
@@ -1,3 +1,10 @@
-module Addition();
+module Addition(
+    input logic [7:0] a, b,
+    output logic [7:0] y,
+    outputl logic overflow
+);
+
+assign y = a + b;
+assign overflow = y < a;
 
 endmodule
--- Subtraction.sv
+++ Subtraction.sv
@@ -1,3 +1,9 @@
-module Subtraction();
+module Subtraction(
+    input logic [7:0] a, b,
+    output logic [7:0] y
+);
+
+logic [7:0] twos_complement_b = ~b + 1'b1;
+assign y = a + twos_complement_b;
 
 endmodule
