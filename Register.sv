51ef8bd1b06f2c526fb0cf741c025cc78d6ef2ae
Anthony Nguyen <coonom2357@gmail.com>
2026-05-26 09:04:17 -0700

    fixed register

--- Register.sv
+++ Register.sv
@@ -7,18 +7,10 @@ module Register#(parameter N = 8)(
 );
 
 always_ff @(posedge CLK or negedge reset_n) begin
-<<<<<<< HEAD
     if (!reset_n)
         q <= '0;
     else if (enable)
         q <= d;
-=======
-    if (reset_n === 1'b0) begin
-        q <= 1'h0;
-	end else begin
-		  q <= d;
-	end
->>>>>>> c31d20eecc1ebd90250d5c72ea7864a9bbb1da9e
 end
 
 endmodule
